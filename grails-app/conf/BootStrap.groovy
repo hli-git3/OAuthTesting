48632ea97e7ac4c08aeca5326b8ac01014eeeb4a
hli <hli713@gmail.com>
2013-09-07 11:33:00 -0500

    config spring-security-core plugin.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,15 @@
+import org.blusky.filesafe.Requestmap
+
 class BootStrap {
 
     def init = { servletContext ->
+	    new Requestmap(url: '/timeline', configAttribute: 'ROLE_USER').save()
+	    new Requestmap(url: '/person/*', configAttribute: 'IS_AUTHENTICATED_REMEMBERED').save()
+	    new Requestmap(url: '/post/followAjax', configAttribute: 'ROLE_USER').save()
+	    new Requestmap(url: '/post/addPostAjax', configAttribute: 'ROLE_USER,IS_AUTHENTICATED_FULLY').save()
+	    new Requestmap(url: '/**', configAttribute: 'IS_AUTHENTICATED_ANONYMOUSLY').save()
     }
+
     def destroy = {
     }
 }
